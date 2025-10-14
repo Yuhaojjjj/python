936c6efbfb705826118c6941c439c754bc8310d9
Yuhaojjjj <ye.yuh24@zaragoza.salesianos.edu>
2025-10-14 12:01:11 +0200

    Add files via upload

--- fraseador_pro_max.py
+++ fraseador_pro_max.py
@@ -6,18 +6,22 @@ def letras(frase):
         if frase[i] in letras:
             let += 1
     print ("Tu frase tiene " + str(let) + " letras.")
-def cyv(frase):
+def consonantes(frase):
     i = 0
-    voc = 0
-    vocales = 'aeiouAEIOU'
     cons = 'qwrtypsdfghjklzxcvbnmQWRTYPSDFGHJKLZXCVBNM'
     con = 0
     for i in range (0, len(frase)):
+        if frase[i] in cons :
+            con += 1
+    print ("Tu frase tiene " + str(con) + " consonantes.")
+def vocales(frase):
+    i = 0
+    voc = 0
+    vocales = 'aeiouAEIOU'
+    for i in range (0, len(frase)):
         if frase[i] in vocales :
             voc += 1
-        elif frase[i] in cons:
-            con += 1
-    print ("Tu frase tiene " + str(voc) + " vocales y " + str(con) + " consonantes.")
+    print ("Tu frase tiene " + str(voc) + " vocales.")
 def pal(frase):
     i = 0
     pal = 1
@@ -46,17 +50,19 @@ def encriptar(frase):
             cfr += chr(ord(frase[i]) + 1)
     print ("Tu frase encriptada es: " + cfr + ".")
 def fraseador():
-    print("Menu del dia:\n1. Número de letra\n2. Número de consonantes y vocales\n3. Número de palabras\n4. Número de veces que aparece la letra o\n5. Encripta el mensaje sustituyendo cada letra por la siguiente en elabecedario")
-    menu = raw_input("¿Que funciones querra para de menu? ")
-    frase = raw_input("Dime algo: ")
+    frase = raw_input("Digame algo: ")
+    print("Menu del dia:\n1. Número de letra\n2. Número de consonantes\n3. Número de vocales vocales\n4. Número de palabras\n5. Número de veces que aparece la letra o\n6. Encripta el mensaje sustituyendo cada letra por la siguiente en elabecedario")
+    menu = raw_input("¿Qué quiere comer hoy? ")
     if '1' in menu:
         letras(frase)
     if '2' in menu:
-        cyv(frase)
+        consonantes(frase)
     if '3' in menu:
-        pal(frase)
+        vocales(frase)
     if '4' in menu:
-        os(frase)
+        pal(frase)
     if '5' in menu:
+        os(frase)
+    if '6' in menu:
         encriptar(frase)
 fraseador()
