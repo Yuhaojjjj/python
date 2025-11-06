22ed440e29985e7644ed5ce358f27fbd8c0e2225
Yuhaojjjj <ye.yuh24@zaragoza.salesianos.edu>
2025-11-06 11:29:00 +0100

    Add files via upload

---
+++ treseador_pro_max.py
@@ -0,0 +1,9 @@
+def treseador_pro_max():
+    num = input("Dime un número:")
+    for i in range (1, 4):
+        numf = num - i
+        print numf
+    for i in range (1, 4):
+        numf = num + i
+        print numf
+treseador_pro_max()
